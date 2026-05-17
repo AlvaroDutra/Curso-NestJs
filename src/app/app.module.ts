c3aa0397c672a785e23443f16c7b656a30589812
Alvaro Dutra <dutalvaro@gmail.com>
2026-05-17 18:15:47 -0400

    Adiciona validação das variáveis de ambiente usando @hapi/joi.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecadosModule } from '../recados/recados.module';
 import { UsuariosModule } from '../usuarios/usuarios.module';
-
+import joi from '@hapi/joi';
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: joi.object({
+        DATABASE_TYPE: joi.required(),
+        DATABASE_HOST: joi.required(),
+        DATABASE_PORT: joi.number().default(5432),
+        DATABASE_USERNAME: joi.required(),
+        DATABASE_DATABASE: joi.required(),
+        DATABASE_PASSWORD: joi.required(),
+        DATABASE_AUTOLOADENTITIES: joi.required(),
+        DATABASE_SYNCHRONIZE: joi.required(),
+      }),
+    }),
     TypeOrmModule.forRoot({
       type: process.env.DATABASE_TYPE as 'postgres',
       host: process.env.DATABASE_HOST,
